5d07f405b3dfae66abb2572e6725f42379818b4d
kaelonlloyd <kaelon.lloyd@ntlworld.com>
2014-11-14 14:42:44 +0000

    a
    a

--- ExportToData.m
+++ ExportToData.m
@@ -5,30 +5,30 @@ Desc = GEPNonPCADescriptors;
 StoreCell = cell(M + 1, N + 2);
 
 for i = 1 : M + 1
-    StoreCell{i,1} = strcat('P',num2str(i - 2));
+    StoreCell{i + 1,1} = strcat('P',num2str(i - 1));
 end
 
 
 for i = 1 : N + 1
-    StoreCell{1,i} = strcat('D',num2str(i - 1));
+    StoreCell{1,i + 1} = strcat('D',num2str(i - 1));
 end
 
 DescCell = num2cell(Desc);
 [G GN] = grp2idx(GEPTags);  % Reduce character tags to numeric grouping
-G = 1./G;
+G = 0.999./G;
 StoreCell(2:M+1,2:N+1) = DescCell;
 StoreCell(2:M+1,N+2) = num2cell(G);
 
 % open a file for writing
-fid = fopen('exptable.txt', 'w');
+fid = fopen('exptable.data', 'w');
 
 
 % print values in column order
 % two values appear on each row of the file
 
-fprintf(fid, 'DN \n');
-fprintf(fid, '598 \n');
-fprintf(fid, '417 \n');
+fprintf(fid, 'DY\n');
+fprintf(fid, '7002\n');
+fprintf(fid, '136\n');
 
 for y = 1 : M + 1
     Line = '';
